cf6d2141844b5be96362f14da92f92d06e97276c
MRandyPE <lego13man@gmail.com>
2015-03-03 11:06:33 -0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := exampleblock # should match the name in the AndroidManifest.xml
+LOCAL_MODULE    := moliquids # should match the name in the AndroidManifest.xml
 LOCAL_SRC_FILES := main.cpp
 # search for libraries in the jni/ folder of our project,
 # link to the libraries for printing to logcat, getting methods from libminecraftpe,
