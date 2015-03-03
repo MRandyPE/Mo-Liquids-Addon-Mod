de767de13f070d6f0b2676eb88b25f6706b238a1
MRandyPE <lego13man@gmail.com>
2015-03-03 13:50:44 -0600

    Update R.java

--- gen/com/mrandype/moliquids/mcpeaddon/R.java
+++ gen/com/mrandype/moliquids/mcpeaddon/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package net.muse.blocktemplate.mcpeaddon;
+package com.mrandype.moliquids.mcpeaddon;
 
 public final class R {
     public static final class attr {
